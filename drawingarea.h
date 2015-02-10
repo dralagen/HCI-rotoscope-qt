7b32dffc52870502c2cd5655473793e2d2283895
obloodo <davidperrai@gmail.com>
2015-02-10 18:08:04 +0100

    adding posibility to draw in the main screen

---
+++ drawingarea.h
@@ -0,0 +1,18 @@
+#ifndef DRAWINGAREA_H
+#define DRAWINGAREA_H
+
+#include <QWidget>
+
+class drawingArea : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit drawingArea(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // DRAWINGAREA_H
